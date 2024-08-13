fcc7f80fb25ab70ce91fd88edf172798bbd8e080
afeefnk <connectafeefnk@gmail.com>
2024-08-13 12:13:00 +0530

    Amazon Project

--- data/cart.js
+++ data/cart.js
@@ -1,10 +1,4 @@
-export let cart;
-
-
-loadFromStorage();
-
-export function loadFromStorage() {
-  cart = JSON.parse(localStorage.getItem('cart'));
+export let cart = JSON.parse(localStorage.getItem('cart'));
 
 if(!cart) {
     cart = [{
@@ -16,9 +10,7 @@ if(!cart) {
         quantity: 1,
         deliveryOptionId: '2'
     }];
-}
-
-}
+  }
 
 function saveToStorage() {
     localStorage.setItem('cart', JSON.stringify(cart));
